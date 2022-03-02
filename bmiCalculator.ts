32e51e8fa07d0433ab14536ebb06f7ae460e7e58
Kimi <porthan.kimi@gmail.com>
2022-03-03 00:01:27 +0200

    9.1–9.3 done

---
+++ bmiCalculator.ts
@@ -0,0 +1,13 @@
+const calculateBmi = (heigth: number, weight: number): string => {
+    const bmi = weight / (Math.pow(heigth / 100, 2))
+
+    if (bmi < 18.5) return "Underweight"
+    if (bmi <= 24.9) return "Normal (healthy weight)"
+    if (bmi <= 29.9) return "Overweight"
+    else return "Obese"
+}
+
+const heigth = Number(process.argv[2])
+const weight = Number(process.argv[3])
+
+console.log(calculateBmi(heigth, weight))
